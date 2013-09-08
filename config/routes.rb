2b952d0fc00d5c23d4b73d826ed738b5e6d2276f
SPZTZM <shipei1209@gmail.com>
2013-09-08 18:45:09 -0400

    Feature 1--making new vote

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 VotingSystem::Application.routes.draw do
+  get "vote/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
